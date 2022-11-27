e6e7ad581a054266f339ec9b9fc58fafa85fe9dd
Andrew Gaffney <andrew@agaffney.org>
2022-11-27 15:07:48 -0600

    feat: flesh out query result types
    This was supposed to cover a lot more, but many of the result types are
    either poorly documented or we cannot currently parse due to maps with
    bytestring keys
    
    Fixes #134

--- cmd/go-ouroboros-network/query.go
+++ cmd/go-ouroboros-network/query.go
@@ -101,7 +101,31 @@ func testQuery(f *globalFlags) {
 			fmt.Printf("ERROR: failure querying era history: %s\n", err)
 			os.Exit(1)
 		}
-		fmt.Printf("era-history: %#v", *eraHistory)
+		fmt.Printf("era-history:\n")
+		for eraId, era := range eraHistory {
+			fmt.Printf("id = %d, begin slot/epoch = %d/%d, end slot/epoch = %d/%d, epoch length = %d, slot length (ms) = %d, slots per KES period = %d\n", eraId, era.Begin.SlotNo, era.Begin.EpochNo, era.End.SlotNo, era.End.EpochNo, era.Params.EpochLength, era.Params.SlotLength, era.Params.SlotsPerKESPeriod.Value)
+		}
+	case "protocol-params":
+		protoParams, err := o.LocalStateQuery.Client.GetCurrentProtocolParams()
+		if err != nil {
+			fmt.Printf("ERROR: failure querying protocol params: %s\n", err)
+			os.Exit(1)
+		}
+		fmt.Printf("protocol-params: %#v\n", *protoParams)
+	case "stake-distribution":
+		stakeDistribution, err := o.LocalStateQuery.Client.GetStakeDistribution()
+		if err != nil {
+			fmt.Printf("ERROR: failure querying stake distribution: %s\n", err)
+			os.Exit(1)
+		}
+		fmt.Printf("stake-distribution: %#v\n", *stakeDistribution)
+	case "genesis-config":
+		genesisConfig, err := o.LocalStateQuery.Client.GetGenesisConfig()
+		if err != nil {
+			fmt.Printf("ERROR: failure querying genesis config: %s\n", err)
+			os.Exit(1)
+		}
+		fmt.Printf("genesis-config: %#v\n", *genesisConfig)
 	default:
 		fmt.Printf("ERROR: unknown query: %s\n", queryFlags.flagset.Args()[0])
 		os.Exit(1)
