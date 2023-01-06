8cfbddf8454f7bca75f6d05da023449140e53c71
Andrew Gaffney <andrew@agaffney.org>
2023-01-06 14:54:37 -0600

    test: encode/decode unit tests for localstatequery messages
    Fixes #68

--- protocol/localstatequery/messages_test.go
+++ protocol/localstatequery/messages_test.go
@@ -3,6 +3,7 @@ package localstatequery
 import (
 	"encoding/hex"
 	"github.com/cloudstruct/go-ouroboros-network/protocol"
+	"github.com/cloudstruct/go-ouroboros-network/protocol/common"
 	"github.com/cloudstruct/go-ouroboros-network/utils"
 	"reflect"
 	"testing"
@@ -14,12 +15,67 @@ type testDefinition struct {
 	MessageType uint
 }
 
-// TODO: implement tests for more messages
 var tests = []testDefinition{
+	{
+		CborHex:     "820080",
+		Message:     NewMsgAcquire(common.Point{}),
+		MessageType: MESSAGE_TYPE_ACQUIRE,
+	},
+	{
+		CborHex:     "8101",
+		Message:     NewMsgAcquired(),
+		MessageType: MESSAGE_TYPE_ACQUIRED,
+	},
+	{
+		CborHex:     "820201",
+		Message:     NewMsgFailure(ACQUIRE_FAILURE_POINT_NOT_ON_CHAIN),
+		MessageType: MESSAGE_TYPE_FAILURE,
+	},
+	{
+		CborHex: "8203820082028101",
+		Message: NewMsgQuery(
+			// Current era hard-fork query
+			[]interface{}{
+				uint64(0),
+				[]interface{}{
+					uint64(2),
+					[]interface{}{
+						uint64(1),
+					},
+				},
+			},
+		),
+		MessageType: MESSAGE_TYPE_QUERY,
+	},
+	{
+		CborHex:     "820405",
+		Message:     NewMsgResult([]byte{5}),
+		MessageType: MESSAGE_TYPE_RESULT,
+	},
+	{
+		CborHex:     "8105",
+		Message:     NewMsgRelease(),
+		MessageType: MESSAGE_TYPE_RELEASE,
+	},
+	{
+		CborHex:     "820680",
+		Message:     NewMsgReAcquire(common.Point{}),
+		MessageType: MESSAGE_TYPE_REACQUIRE,
+	},
 	{
 		CborHex:     "8107",
 		Message:     NewMsgDone(),
 		MessageType: MESSAGE_TYPE_DONE,
+	},
+	{
+		CborHex:     "8108",
+		Message:     NewMsgAcquireNoPoint(),
+		MessageType: MESSAGE_TYPE_ACQUIRE_NO_POINT,
+	},
+	{
+		CborHex:     "8109",
+		Message:     NewMsgReAcquireNoPoint(),
+		MessageType: MESSAGE_TYPE_REACQUIRE_NO_POINT,
 	},
 }
 
@@ -36,7 +92,7 @@ func TestDecode(t *testing.T) {
 		// Set the raw CBOR so the comparison should succeed
 		test.Message.SetCbor(cborData)
 		if !reflect.DeepEqual(msg, test.Message) {
-			t.Fatalf("CBOR did not decode to expected message object\n  got: %#v\n  wanted: %#v", msg, test.Message)
+			t.Fatalf("CBOR did not decode to expected message object\n  got:    %#v\n  wanted: %#v", msg, test.Message)
 		}
 	}
 }
@@ -49,7 +105,7 @@ func TestEncode(t *testing.T) {
 		}
 		cborHex := hex.EncodeToString(cborData)
 		if cborHex != test.CborHex {
-			t.Fatalf("message did not encode to expected CBOR\n  got: %s\n  wanted: %s", cborHex, test.CborHex)
+			t.Fatalf("message did not encode to expected CBOR\n  got:    %s\n  wanted: %s", cborHex, test.CborHex)
 		}
 	}
 }
