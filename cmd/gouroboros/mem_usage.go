daaabff7e057b0363c12063a7f40c29663662474
Chris Gianelloni <wolf31o2@blinklabs.io>
2025-03-14 15:12:09 -0400

    chore: inline comment to appease golangci-lint (#952)
    Signed-off-by: Chris Gianelloni <wolf31o2@blinklabs.io>

--- cmd/gouroboros/mem_usage.go
+++ cmd/gouroboros/mem_usage.go
@@ -1,4 +1,4 @@
-// Copyright 2023 Blink Labs Software
+// Copyright 2025 Blink Labs Software
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -19,8 +19,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	// #nosec G108
-	_ "net/http/pprof"
+	_ "net/http/pprof" // #nosec G108
 	"os"
 	"runtime"
 	"runtime/pprof"
