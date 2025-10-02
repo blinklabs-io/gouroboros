ed943a7274eb7e4adcd0fe86994d9f41803a6626
Chris Gianelloni <wolf31o2@blinklabs.io>
2025-10-02 19:47:25 -0400

    fix(ledger): use transactions for leios endorser blocks
    Since Leios Endorser Blocks do not have the transaction contents in
    their CBOR representation, we do not have a container for them. Add
    `transactions` to allow us to store them in the block once resolved for
    later retrieval, such as via the `Transactions()` func.
    
    Signed-off-by: Chris Gianelloni <wolf31o2@blinklabs.io>

--- ledger/leios/leios.go
+++ ledger/leios/leios.go
@@ -64,7 +64,8 @@ type LeiosEndorserBlock struct {
 	cbor.DecodeStoreCbor
 	cbor.StructAsArray
 	hash         *common.Blake2b256
-	TxReferences []common.TxReference
+	transactions []common.Transaction
+	TxReferences []common.TxReference `cbor:"0,keyasint"`
 }
 
 func (h *LeiosBlockHeader) UnmarshalCBOR(cborData []byte) error {
@@ -153,9 +154,7 @@ func (b *LeiosEndorserBlock) PrevHash() common.Blake2b256 {
 }
 
 func (b *LeiosEndorserBlock) Transactions() []common.Transaction {
-	// TODO: convert TxReferences into []Transaction
-	// TxReferences []common.TxReference
-	return []common.Transaction{}
+	return b.transactions
 }
 
 func (b *LeiosEndorserBlock) Utxorpc() (*utxorpc.Block, error) {
