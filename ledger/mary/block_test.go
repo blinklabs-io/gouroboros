20484bce1587681c4b60ea66ceaac7498a05143f
Chris Gianelloni <wolf31o2@blinklabs.io>
2025-12-01 11:43:54 -0500

    fix(ledger): custom cbor marshal for nativescript (#1294)
    Signed-off-by: Chris Gianelloni <wolf31o2@blinklabs.io>

--- ledger/mary/block_test.go
+++ ledger/mary/block_test.go
@@ -48,6 +48,8 @@ func TestMaryBlock_CborRoundTrip_UsingCborEncode(t *testing.T) {
 	if err != nil {
 		t.Fatalf("Failed to unmarshal CBOR data into MaryBlock: %v", err)
 	}
+	// Reset stored CBOR to nil
+	block.SetCbor(nil)
 
 	// Re-encode using the cbor Encode function
 	encoded, err := cbor.Encode(block)
