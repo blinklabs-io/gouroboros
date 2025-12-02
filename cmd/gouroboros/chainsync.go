49c7882288b0174866219361a7389d1778ee8be5
Chris Gianelloni <wolf31o2@blinklabs.io>
2025-12-02 08:06:25 -0500

    fix(cmd): use chainsync pipeline limit 10 (#1297)
    Signed-off-by: Chris Gianelloni <wolf31o2@blinklabs.io>

--- cmd/gouroboros/chainsync.go
+++ cmd/gouroboros/chainsync.go
@@ -128,6 +128,7 @@ func buildChainSyncConfig() chainsync.Config {
 	return chainsync.NewConfig(
 		chainsync.WithRollBackwardFunc(chainSyncRollBackwardHandler),
 		chainsync.WithRollForwardFunc(chainSyncRollForwardHandler),
+		chainsync.WithPipelineLimit(10),
 	)
 }
 
