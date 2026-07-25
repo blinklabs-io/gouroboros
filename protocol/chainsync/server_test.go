8c88bf4af875fbfd96e472a7eed575ef107aa3ec
Chris Gianelloni <wolf31o2@blinklabs.io>
2026-07-25 04:56:18 -0400

    perf(protocol): fix chainsync rollforward debug log (#1907)
    Signed-off-by: Chris Gianelloni <wolf31o2@blinklabs.io>

--- protocol/chainsync/server_test.go
+++ protocol/chainsync/server_test.go
@@ -15,9 +15,13 @@
 package chainsync
 
 import (
+	"bytes"
+	"log/slog"
 	"testing"
 
+	pcommon "github.com/blinklabs-io/gouroboros/protocol/common"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestHandleRequestNext_Callback(t *testing.T) {
@@ -51,4 +55,41 @@ func TestHandleRequestNext_NilCallback(t *testing.T) {
 
 	assert.Error(t, err, "expected an error due to nil callback")
 	assert.EqualError(t, err, expectedError)
+}
+
+func TestLogRollForwardDoesNotLogBlockData(t *testing.T) {
+	var logOutput bytes.Buffer
+	logger := slog.New(slog.NewTextHandler(
+		&logOutput,
+		&slog.HandlerOptions{Level: slog.LevelDebug},
+	))
+	tip := Tip{
+		Point: pcommon.Point{
+			Slot: 42,
+			Hash: []byte{0xca, 0xfe},
+		},
+		BlockNumber: 24,
+	}
+
+	logRollForward(
+		logger,
+		5,
+		[]byte{0xde, 0xad, 0xbe, 0xef},
+		tip,
+		"connection-id",
+	)
+
+	output := logOutput.String()
+	require.NotEmpty(t, output)
+	assert.Contains(t, output, "msg=\"calling RollForward\"")
+	assert.Contains(t, output, "block_type=5")
+	assert.Contains(t, output, "block_size=4")
+	assert.Contains(t, output, "tip_slot=42")
+	assert.Contains(t, output, "tip_block_number=24")
+	assert.NotContains(
+		t,
+		output,
+		"deadbeef",
+		"serialized block data should not be logged",
+	)
 }
