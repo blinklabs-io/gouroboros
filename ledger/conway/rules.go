d779412780c3ef8d4cf7fa09f2ce77164f4ae36e
Chris Gianelloni <wolf31o2@blinklabs.io>
2026-09-03 15:05:31 -0400

    fix(ledger): preserve Conway pool rule ordering
    Signed-off-by: Chris Gianelloni <wolf31o2@blinklabs.io>

--- ledger/conway/rules.go
+++ ledger/conway/rules.go
@@ -323,11 +323,11 @@ var UtxoValidationRules = common.ComposeUtxoValidationRules(
 	),
 	common.Phase2ValidUtxoValidationRules(
 		UtxoValidateDelegation, UtxoValidateWithdrawals, UtxoValidateCertificateDeposits,
-		UtxoValidatePoolCertificates,
 		UtxoValidateCommitteeCertificates, UtxoValidateUnknownVoters,
 		UtxoValidateUnknownGovActionIds, UtxoValidateVotingOnExpiredGovAction,
 		UtxoValidateBootstrapVotingRestrictions, UtxoValidateStakePoolVotingRestrictions,
 		UtxoValidateCCVotingRestrictions, UtxoValidateRefScriptSizePerTx,
+		UtxoValidatePoolCertificates,
 	),
 )
 
