4a826335c05a896739c46c19cc784d61f7811f10
Chris Gianelloni <wolf31o2@blinklabs.io>
2026-09-05 11:26:21 -0400

    fix(muxer): remove unrelated ledger change
    Signed-off-by: Chris Gianelloni <wolf31o2@blinklabs.io>

--- ledger/common/tx.go
+++ ledger/common/tx.go
@@ -370,11 +370,7 @@ func EncodeTransactionBodyWithValidityIntervalUpperBound(
 	treasuryValue := body.CurrentTreasuryValue()
 	preserveTreasuryZero := TransactionCurrentTreasuryValuePresent(body) &&
 		treasuryValue != nil && treasuryValue.Sign() == 0
-	networkIdValue, networkIdPresent := body.(interface {
-		NetworkIdPresent() bool
-	})
-	preserveNetworkIdZero := networkIdPresent && networkIdValue.NetworkIdPresent()
-	if !preserveUpperBoundZero && !preserveTreasuryZero && !preserveNetworkIdZero {
+	if !preserveUpperBoundZero && !preserveTreasuryZero {
 		return cborData, nil
 	}
 	bodyFields := make(map[uint]cbor.RawMessage)
@@ -394,17 +390,6 @@ func EncodeTransactionBodyWithValidityIntervalUpperBound(
 			return nil, err
 		}
 		bodyFields[21] = encodedTreasuryValue
-	}
-	if preserveNetworkIdZero {
-		if networkIdValue, ok := body.(interface{ TransactionNetworkId() *uint8 }); ok {
-			if value := networkIdValue.TransactionNetworkId(); value != nil && *value == 0 {
-				encodedNetworkId, err := cbor.Encode(uint8(0))
-				if err != nil {
-					return nil, err
-				}
-				bodyFields[15] = encodedNetworkId
-			}
-		}
 	}
 	return cbor.Encode(bodyFields)
 }
