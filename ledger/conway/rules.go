4d9f898a2a88f91f6156cf8a922d2e0ecbce477b
Chris Gianelloni <wolf31o2@blinklabs.io>
2026-09-11 17:13:49 -0400

    fix(ledger): propagate DRep lookup errors
    Signed-off-by: Chris Gianelloni <wolf31o2@blinklabs.io>

--- ledger/conway/rules.go
+++ ledger/conway/rules.go
@@ -3444,7 +3444,10 @@ func UtxoValidateDelegation(
 			}
 			// Check ledger state
 			reg, err := ls.DRepRegistration(cred)
-			return err == nil && reg != nil, nil
+			if err != nil {
+				return false, err
+			}
+			return reg != nil, nil
 		default:
 			return false, InvalidDRepTypeError{DrepType: drep.Type}
 		}
