68931b88b9faec96788758516130a58fde430083
Chris Gianelloni <wolf31o2@blinklabs.io>
2026-09-14 19:12:57 -0400

    fix(localstatequery): complete acquisition transitions (#2320)
    Signed-off-by: Chris Gianelloni <wolf31o2@blinklabs.io>

--- protocol/localstatequery/server.go
+++ protocol/localstatequery/server.go
@@ -105,7 +105,11 @@ func (s *Server) messageHandler(msg protocol.Message) error {
 		err = s.handleReAcquire(msg)
 	case MessageTypeAcquireVolatileTip:
 		err = s.handleAcquire(msg)
+	case MessageTypeAcquireImmutableTip:
+		err = s.handleAcquire(msg)
 	case MessageTypeReacquireVolatileTip:
+		err = s.handleReAcquire(msg)
+	case MessageTypeReacquireImmutableTip:
 		err = s.handleReAcquire(msg)
 	case MessageTypeDone:
 		s.handleDone()
@@ -159,6 +163,7 @@ func (s *Server) handleAcquire(msg protocol.Message) error {
 		} else {
 			return err
 		}
+		return nil
 	}
 	respMsg := NewMsgAcquired()
 	if err := s.SendMessage(respMsg); err != nil {
@@ -265,6 +270,11 @@ func (s *Server) handleReAcquire(msg protocol.Message) error {
 		} else {
 			return err
 		}
+		return nil
+	}
+	respMsg := NewMsgAcquired()
+	if err := s.SendMessage(respMsg); err != nil {
+		return err
 	}
 	return nil
 }
