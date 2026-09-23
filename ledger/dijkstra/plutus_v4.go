b99536093c278326e81952f21f5f601ad3dd7096
Chris Gianelloni <wolf31o2@blinklabs.io>
2026-09-23 04:59:10 -0400

    fix: match current Dijkstra Plutus V4 context
    Closes #2478
    Closes #2479
    
    Signed-off-by: Chris Gianelloni <wolf31o2@blinklabs.io>

--- ledger/dijkstra/plutus_v4.go
+++ ledger/dijkstra/plutus_v4.go
@@ -44,18 +44,6 @@ func dijkstraPlutusV4Context(
 	if err != nil {
 		return nil, err
 	}
-	if guarding, ok := purpose.(script.ScriptPurposeGuarding); ok &&
-		level.subTxIndex == nil {
-		topTxInfo, err := dijkstraTopTxInfoV4(level, guarding.Guard)
-		if err != nil {
-			return nil, err
-		}
-		scriptInfo = data.NewConstr(
-			6,
-			data.NewInteger(new(big.Int).SetUint64(uint64(key.Index))),
-			data.NewConstr(0, topTxInfo),
-		)
-	}
 	_ = purposeData // The purpose is represented in txInfoRedeemers.
 	return data.NewConstr(
 		0,
@@ -136,7 +124,7 @@ func dijkstraTxInfoV4(level dijkstraScriptLevel) (data.PlutusData, error) {
 	return data.NewConstr(
 		0,
 		data.NewByteString(level.tx.Id().Bytes()),
-		dijkstraOptionalIndex(level.subTxIndex),
+		dijkstraOptionalIndex(nil),
 		data.NewList(inputs...),
 		data.NewList(referenceInputs...),
 		data.NewList(outputs...),
